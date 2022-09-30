571c8750332fa3aa63639e833c6e3df12c7f3ebf
nyteghost <nytemmo@gmail.com>
2022-09-30 19:57:25 -0400

    Update notes_for_fillin.py

--- VeritickAPP/scripts/notes_for_fillin.py
+++ VeritickAPP/scripts/notes_for_fillin.py
@@ -4,7 +4,7 @@ class Default(dict):
 
 
 uMAD = """
-Thank you for contacting SCA regarding your GCA equipment.
+Thank you for contacting Place regarding your SomeSchool equipment.
 
 Our records indicate {need} assigned to {student_name} {thatwerenot} replaced. 
 
@@ -18,24 +18,21 @@ The {label} should arrive at your email {email} inbox this afternoon between 3:3
 
 Please note, all other labels on the package MUST be covered or marked out to prevent the package from being returned to you.
 
-If you discover any error in the shipment information, please call us at 404.334.4790 Option 8, or respond to this email.
+If you discover any error in the shipment information, please call us at <Phone Number>, or respond to this email.
 
 Thank you again for allowing us to assist you with your GCA equipment needs.
 
 Best,
 
-SCA Shipping Team
-Southeastern Computer Associates, LLC (SCA)
-Office: 404-334-4790, Option 8
-support@sca-atl.com"""
 
+"""
 
 por_ticket = """
 Hello,
 
-Thank you for contacting SCA regarding your GCA equipment. Unfortunately, the address provided at the time that the ticket was placed does not match what we have been provided by GCA as your authorized shipping address.
+Thank you for contacting Place regarding your SomeSchool equipment. Unfortunately, the address provided at the time that the ticket was placed does not match what we have been provided by GCA as your authorized shipping address.
 
-To complete an address change, please submit the address  along with proof of residency to addresschanges@georgiacyber.org.  Proof of residency can include any of the following documents:
+To complete an address change, please submit the address  along with proof of residency to <Email address>.  Proof of residency can include any of the following documents:
 A current and complete Lease Agreement (showing signature date of lease, terms, name and address)
 Most recent Mortgage Statement
 Most recent Utility Bill showing the service address and issued within 60 days.
@@ -45,16 +42,13 @@ Please note that a cell phone bill is not an acceptable Proof of Residency becau
 
 If you do not have a Proof of Residency document in your name, an Affidavit of Residence Form may be submitted, completed by you and the home owner/leasee. This form must be notarized.
 
-If you do not have any of the above proof of residency documentation, please contact addresschanges@georgiacyber.org, 470-400-788 
+If you do not have any of the above proof of residency documentation, please contact <Email>, <Phone>
 
 Best, 
  
-SCA Shipping Team 
-Southeastern Computer Associates, LLC (SCA) 
-404-334-4790 Option 8 
-shipping@scacloud.com | www.sca-atl.com """
+ """
 
 
 mu_search = """
-SELECT * FROM GCAAssetMGMT_2_0.Ship.WorldshipData WHERE Label_Method IS NOT NULL AND Date > '{date_to_search}' and Ref2 like '{STID}%'
+SELECT * FROM SomeSchoolAssetMGMT_2_0.Ship.WorldshipData WHERE Label_Method IS NOT NULL AND Date > '{date_to_search}' and Ref2 like '{STID}%'
 """
